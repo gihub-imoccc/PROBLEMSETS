d7a5b31d04fb95f9304d9d7d66614ac735d44e3f
ต๋อย1 <thapat.suk@gmail.com>
2026-09-20 21:46:44 +0700

    Add files via upload

---
+++ coincombinationsi.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll MOD = 1e9 + 7;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    ll n, sum; cin >> n >> sum;
+    vector<ll> coin(n); for(auto & e : coin) cin >> e;
+    
+    vector<ll> dp(sum + 1);
+
+    dp[0] = 1;
+    
+    for(int i = 1; i <= sum; i++) {
+        for(auto e : coin) {
+            if(i - e >= 0) {
+                dp[i] += dp[i - e];
+            }
+        }
+        dp[i] %= MOD;
+    }
+    cout << dp[sum];
+
+    return 0;
+}
